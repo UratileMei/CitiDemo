20db39c518b81dc66a32f87575d1ac311a7af68d
UratileMei <uisleymei@gmail.com>
2025-10-14 13:31:38 +0200

    Created activity_register.xml and RegisterActivity.java.

---
+++ app/src/main/java/com/example/citidemo/activities/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.citidemo.activities;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.citidemo.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linearLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
