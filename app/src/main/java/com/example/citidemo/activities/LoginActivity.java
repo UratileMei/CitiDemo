8fa04d0782e44aab05e9cde375fdb06ee43abb28
UratileMei <uisleymei@gmail.com>
2025-10-15 11:35:51 +0200

    Wrote temporary code for switching to new screen when sign up is clicked. To Implement fragment.

--- app/src/main/java/com/example/citidemo/activities/LoginActivity.java
+++ app/src/main/java/com/example/citidemo/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.citidemo.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,20 +8,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.example.citidemo.R;
+import com.google.android.material.tabs.TabItem;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText tilEmail;
+    private TabItem sign_up_tab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        tilEmail = findViewById(R.id.tilEmail);
+        sign_up_tab = findViewById(R.id.sign_up_tab);
+
+        sign_up_tab.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
 }
